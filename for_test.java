bb2fb38b95c46b1e071e82e715a117c00a6bbbae
Vector <adnanraashid448@gmail.com>
2023-04-09 19:05:48 -0700

    to check if its committed

--- for_test.java
+++ for_test.java
@@ -1,7 +1,7 @@
 public class for_test{
     public static void main(String[]args){
         String name = "adnan name";
-        int me = 5;
+        int me = 4;
         System.out.println(me);
         System.out.print(name);
     }
