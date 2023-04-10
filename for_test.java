151cb7ebe1df7dfc240c916ab9646b644a852a4d
Vector <adnanraashid448@gmail.com>
2023-04-09 18:40:00 -0700

    first version of retry git_5

---
+++ for_test.java
@@ -0,0 +1,8 @@
+public class for_test{
+    public static void main(String[]args){
+        String name = "adnan name";
+        int me = 5;
+        System.out.println(me);
+        System.out.print(name);
+    }
+}
---
+++ new_again.py
@@ -0,0 +1,7 @@
+print('what is this again?')
+#print(a)
+a  = ' whats your name?'
+print('welcome to git')
+b = 'yet another attempt to change again'
+c = "another test on the version performance"
+print(c)
